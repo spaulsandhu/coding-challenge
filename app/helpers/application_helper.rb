36955421aa8a0bde4be4196baf336ecf40f89e02
Paul Sandhu <spaulsandhu@gmail.com>
2020-07-20 20:00:52 -0400

    Add helper methods for views
    This adds a few helper methods for our views:
    
    1) The formatted_date helper spits out a human readable date for a given DateTime/Time object. This method is used in the post index page and various other places.
    
    2) The display_inline_form_errors method is used to inject inline errors for a given form. I've used this pattern on various projects in the past, especially when I'm not leveraging a framework like Bootstrap and its ilk.
    
    3) add_or_edit -> A relatively simple method that just changes a single string depending on if an object has been saved to the database and is thus being edited, or is a new record and requires slightly different verbiage.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def formatted_date(date)
+    date.strftime("%A, %B #{date.day.ordinalize} %Y")
+  end
+
+  def display_inline_form_errors(model, field)
+    if model.errors[field].any?
+      model.errors.full_messages_for(field).collect do |error|
+        content_tag(:p, error.html_safe, class: 'error-text')
+      end
+    else
+      []
+    end.join.html_safe
+  end
+
+  def add_or_edit(object)
+    object.persisted? ? "Edit" : "Add"
+  end
 end
